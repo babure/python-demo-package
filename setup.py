e38731a8063db63fac87b4eee6eee97fa26e59e4
babure <babu.reddy@avekshaa.com>
2023-12-12 15:43:13 +0530

    Adding the redis integration

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='myorg_connectors',
+    name='common-sniper-lib',
     version='0.1',
     packages=find_packages(),
     install_requires=[
         'elasticsearch==7.12.0',
         'pymongo==3.13',
+        'redis'
     ],
 )
